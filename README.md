2efe9c071f85659bdb3e023871c55433d2a253bd
Mashrur Hossain <mashrur.hossain@gmail.com>
2020-03-16 14:53:46 +0100

    Add article model

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-MODELS
+Article model - 
+
+- Read
+- Update
+- Delete
---
+++ app/models/article.rb
@@ -0,0 +1,4 @@
+class Article < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 6, maximum: 100 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+end
