83360929b5b1feb45491ecb7c9f8aa9285ab0c64
Mashrur Hossain <mashrur.hossain@gmail.com>
2020-03-18 16:31:42 +0100

    Add validation error messages display and flash messaging

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-The front-end: Creating articles 
-
-- Route
-- Controller#action
-- Model/db table
-- View
+The front-end: Error messages and flash helper (hash)
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,13 +9,17 @@ class ArticlesController < ApplicationController
   end
 
   def new
-
+    @article = Article.new
   end
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-    redirect_to @article
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
 end
