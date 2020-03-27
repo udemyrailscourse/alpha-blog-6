0cc39c1815eff9e591356066ed06f518ed7f9ec8
Mashrur Hossain <mashrur.hossain@gmail.com>
2020-03-27 16:41:20 +0100

    Style validation and flash messages

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
