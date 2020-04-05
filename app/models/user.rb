1834571a5a219fb3347f0a3d658a7a8e89607a78
Mashrur Hossain <mashrur.hossain@gmail.com>
2020-04-05 11:38:30 +0200

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
-
+  validates :username, presence: true, 
+                      uniqueness: { case_sensitive: false }, 
+                      length: { minimum: 3, maximum: 25 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, 
+                      uniqueness: { case_sensitive: false }, 
+                      length: { maximum: 105 },
+                      format: { with: VALID_EMAIL_REGEX }
 end
