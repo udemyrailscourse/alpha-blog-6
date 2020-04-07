02ad866b53febcee81c760fb118aa6124bea4692
Mashrur Hossain <mashrur.hossain@gmail.com>
2020-04-07 11:13:17 +0200

    Convert emails to downcase before saving

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   has_many :articles
   validates :username, presence: true, 
                       uniqueness: { case_sensitive: false }, 
