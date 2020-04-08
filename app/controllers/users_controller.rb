57eb602a0e582d31e6ecc19efbddf3681745a2be
Mashrur Hossain <mashrur.hossain@gmail.com>
2020-04-08 13:43:08 +0200

    Add new users signup form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
