51c2ec4541a6340910f07211445ac13af21d0c5c
Mashrur Hossain <mashrur.hossain@gmail.com>
2020-04-09 16:55:33 +0200

    Add users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
   def new
     @user = User.new
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def gravatar_for(user, options = { size: 80})
+    email_address = user.email.downcase
+    hash = Digest::MD5.hexdigest(email_address)
+    size = options[:size]
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
+  end
+
 end
