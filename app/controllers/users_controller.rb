51899366862a4f9509ffad71697af823cd0281d9
Mashrur Hossain <mashrur.hossain@gmail.com>
2020-04-13 13:07:00 +0200

    Modify layout and redirect link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated"
-      redirect_to articles_path
+      redirect_to @user
     else
       render 'edit'
     end
