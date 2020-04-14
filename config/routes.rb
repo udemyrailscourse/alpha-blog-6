386d8f6a61fa9ab415c65baa63d0ca1225f6c32f
Mashrur Hossain <mashrur.hossain@gmail.com>
2020-04-14 16:50:12 +0200

    Add users login form

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   resources :articles
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
