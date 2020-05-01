6f18eb452514de8d6f854e05faa3bc306adecd26
Mashrur Hossain <mashrur.hossain@gmail.com>
2020-05-01 16:20:18 +0200

    Add boolean admin user field to app

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_07_140656) do
+ActiveRecord::Schema.define(version: 2020_05_01_140013) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_04_07_140656) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
