3e29dbf0ffa8aa77086365a3b1294714de19a3f2
Mashrur Hossain <mashrur.hossain@gmail.com>
2020-05-14 15:41:21 +0200

    Build categories index integration test and view

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -15,7 +15,7 @@ class CategoriesController < ApplicationController
   end
 
   def index
-
+    @categories = Category.paginate(page: params[:page], per_page: 5)
   end
 
   def show
---
+++ test/integration/list_categories_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class ListCategoriesTest < ActionDispatch::IntegrationTest
+  def setup
+    @category = Category.create(name: "Sports")
+    @category2 = Category.create(name: "Travel")
+  end
+
+  test "should show categories listing" do
+    get '/categories'
+    assert_select "a[href=?]", category_path(@category), text: @category.name
+    assert_select "a[href=?]", category_path(@category2), text: @category2.name
+  end
+end
