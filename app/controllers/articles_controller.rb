d307ea65a16e93aa700fa6a691694cd82a38fca1
Mashrur Hossain <mashrur.hossain@gmail.com>
2020-05-28 12:36:51 +0200

    Add article category association when creating articles from UI

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -49,7 +49,7 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title, :description)
+    params.require(:article).permit(:title, :description, category_ids: [])
   end
 
   def require_same_user
