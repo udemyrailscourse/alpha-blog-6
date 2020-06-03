309e5fae77e91ba7199f4fef7c8b28b2985f9e1a
Mashrur Hossain <mashrur.hossain@gmail.com>
2020-06-03 14:39:49 +0200

    Add categories edit functionality with admin requirement

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -15,6 +15,20 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Category name updated successfully"
+      redirect_to @category
+    else
+      render 'edit'
+    end
+  end
+
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
   end
